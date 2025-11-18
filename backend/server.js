4ba35b72cc61fdd39b098707faa5cf5e06aec2f0
Esha Koladiya <eshakoladiya2020@gmail.com>
2025-11-18 11:12:05 +0530

    project completed

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ await connectDB()
 await connectCloudinary()
 
 //Allow multiple origins
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173', 'https://grocery-martf-git-main-es-projects-e3fff532.vercel.app']
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
